04543b63d8034dcaa99b4e749706cfca18e83477
IT21815096-Kavindu2MK <it21815096@my.sliit.lk>
2025-10-27 22:27:34 +0530

    Added server and controller util

---
+++ SpringBootBackend/src/main/java/signlens/backend/util/common/REST_CONTROLLER_URL.java
@@ -0,0 +1,16 @@
+package signlens.backend.util.common;
+
+public class REST_CONTROLLER_URL {
+
+	
+	public static final String USER_REGISTRATION = "/user/userRegistration";
+	public static final String DETECT_DYNAMIC_SIGN = "/sign/detectDynamicSign";
+	public static final String AUDIO_TO_SIGN = "/sign/audioToSign";
+	public static final String VIDEO_TO_SIGN = "/sign/videoToSign";
+	public static final String VOCAL_TRAINING = "/sign/vocalTraining";
+	public static final String GET_ALL_LESSONS = "/lessons/getAllLessons";
+	public static final String START_QUIZ = "/quiz/startQuiz";
+	public static final String GET_QUIZ = "/quiz/getQuiz";
+	public static final String GET_URL = "/sign/getURL";
+	
+}
